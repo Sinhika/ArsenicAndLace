191ad26f9c5e725c0fe32448627163a697e0eb8a
Sinhika <sinhika@republicofnewhome.org>
2020-12-27 12:10:21 -0600

    added script to generate blockstates & models

---
+++ scripts/generate_blockstates_and_models.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# scripts are from mod_utils project on my GitHub.
+
+ID='arsenic'
+NAME='ArsenicAndLace'
+TOPDIR=`pwd`
+PROJNAME=`basename $TOPDIR`
+if [ $PROJNAME != $NAME ]; then
+    echo "Running in wrong directory!"
+    exit 1
+fi
+
+# blockstates
+TARGETDIR=${TOPDIR}/src/main/resources/assets/${ID}/blockstates
+if [ ! -d $TARGETDIR ]; then
+    mkdir -p $TARGETDIR
+fi
+cd $TARGETDIR
+
+# make simple blockstates
+gen_blockstate_jsons.py --type=simple arsenic_block
+gen_blockstate_jsons.py --type=simple tenebrium_block
+gen_blockstate_jsons.py --type=simple arsenide_gold_block
+gen_blockstate_jsons.py --type=simple arsenide_bronze_block
+
