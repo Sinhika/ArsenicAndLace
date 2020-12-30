b894d93e629af7e344872d04ff7ce6d7c98f89f1
Sinhika <sinhika@republicofnewhome.org>
2020-12-30 12:33:16 -0600

    added generated block models

--- scripts/generate_blockstates_and_models.sh
+++ scripts/generate_blockstates_and_models.sh
@@ -25,3 +25,15 @@ gen_blockstate_jsons.py --type=simple arsenide_gold_block
 gen_blockstate_jsons.py --type=simple arsenide_bronze_block
 gen_blockstate_jsons.py --type=pressure_plate arsenic_plate
 
+# models
+TARGETDIR=${TOPDIR}/src/main/resources/assets/${ID}/models
+if [ ! -d $TARGETDIR ]; then
+    mkdir -p $TARGETDIR
+fi
+cd $TARGETDIR
+
+gen_model_jsons.py --type=block arsenic_block
+gen_model_jsons.py --type=block tenebrium_block 
+gen_model_jsons.py --type=block arsenide_gold_block
+gen_model_jsons.py --type=block arsenide_bronze_block
+
