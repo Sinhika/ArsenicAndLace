079973c06d0e87f3ecc833858181d3ab24899af3
Sinhika <sinhika@republicofnewhome.org>
2021-02-12 12:08:34 -0600

    added ingot/dust/chunk items; updated mods.toml, added 'material' translations, created ArmorMaterial

---
+++ src/main/java/mod/akkamaddi/arsenic/content/ArsenicArmorMaterial.java
@@ -0,0 +1,104 @@
+package mod.akkamaddi.arsenic.content;
+
+import java.util.function.Supplier;
+
+import mod.akkamaddi.arsenic.init.ModItems;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.Items;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+
+public enum ArsenicArmorMaterial implements IArmorMaterial
+{
+    OLD_LACE ("arsenic:old_lace", 4, new int[] { 1, 1, 1, 1 }, 24, 
+            SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 0.0F, 
+            ()-> { return Ingredient.fromItems(Items.STRING);}, 0.0F),
+    ARSENIDE_BRONZE("arsenic:arsenide_bronze", 16, new int[] { 1, 5, 3, 3 }, 9,
+              SoundEvents.ITEM_ARMOR_EQUIP_GOLD, 0.0F, 
+              ()-> { return Ingredient.fromItems(ModItems.arsenide_bronze_ingot.get());}, 0.0F),
+    
+    ARSENIDE_GOLD("arsenic:arsenide_gold", 12, new int[] { 2, 6, 3, 1 }, 22,
+            SoundEvents.ITEM_ARMOR_EQUIP_GOLD, 0.0F, 
+            ()-> { return Ingredient.fromItems(ModItems.arsenide_gold_ingot.get());}, 0.0F),
+ 
+    TENEBRIUM("arsenic:tenebrium", 52, new int[] { 4, 8, 8, 4 }, 17,
+            SoundEvents.ITEM_ARMOR_EQUIP_IRON, 1.0F, 
+            ()-> { return Ingredient.fromItems(ModItems.tenebrium_ingot.get());}, 0.0F);
+             
+    private static final int[] MAX_DAMAGE_ARRAY = new int[]{13, 15, 16, 11};
+    private final String name;
+    private final int maxDamageFactor;
+    private final int[] damageReductionAmountArray;
+    private final int enchantability;
+    private final SoundEvent soundEvent;
+    private final float toughness;
+    private final LazyValue<Ingredient> repairMaterial;
+    private final float knockbackResist;
+
+    private ArsenicArmorMaterial(String nameIn, int maxDamageIn, int[] drAmtArray,
+            int enchantabilityIn, SoundEvent soundIn,
+            float toughnessIn,
+            Supplier<Ingredient> repairMatIn, float knockbackIn)
+    {
+        name = nameIn;
+        maxDamageFactor = maxDamageIn;
+        damageReductionAmountArray = drAmtArray;
+        enchantability = enchantabilityIn;
+        soundEvent = soundIn;
+        toughness = toughnessIn;
+        repairMaterial = new LazyValue<>(repairMatIn);
+        knockbackResist = knockbackIn;
+    }
+
+    @Override
+    public int getDurability(EquipmentSlotType slotIn)
+    {
+        return MAX_DAMAGE_ARRAY[slotIn.getIndex()] * this.maxDamageFactor;
+    }
+
+    @Override
+    public int getDamageReductionAmount(EquipmentSlotType slotIn)
+    {
+        return this.damageReductionAmountArray[slotIn.getIndex()];
+    }
+
+    @Override
+    public int getEnchantability()
+    {
+        return this.enchantability;
+    }
+
+    @Override
+    public SoundEvent getSoundEvent()
+    {
+        return this.soundEvent;
+    }
+
+    @Override
+    public Ingredient getRepairMaterial()
+    {
+        return this.repairMaterial.getValue();
+    }
+
+    @Override
+    public String getName()
+    {
+        return this.name;
+    }
+
+    @Override
+    public float getToughness()
+    {
+        return this.toughness;
+    }
+
+    @Override
+    public float getKnockbackResistance()
+    {
+        return this.knockbackResist;
+    }
+
+} // end-class
--- src/main/java/mod/akkamaddi/arsenic/init/ModItems.java
+++ src/main/java/mod/akkamaddi/arsenic/init/ModItems.java
@@ -2,6 +2,7 @@ package mod.akkamaddi.arsenic.init;
 
 import mod.akkamaddi.arsenic.ArsenicAndLace;
 import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -20,5 +21,54 @@ public final class ModItems
 {
     public static final DeferredRegister<Item> ITEMS = 
             DeferredRegister.create(ForgeRegistries.ITEMS, ArsenicAndLace.MODID);
-
+    
+    // ingots, chunks, and dusts
+    public static final RegistryObject<Item> arsenic_ingot = ITEMS.register("arsenic_ingot", 
+            ()-> new Item(new Item.Properties().group(ModTabGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> arsenic_nugget = ITEMS.register("arsenic_nugget", 
+            ()-> new Item(new Item.Properties().group(ModTabGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> arsenic_dust = ITEMS.register("arsenic_dust", 
+            ()-> new Item(new Item.Properties().group(ModTabGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> realgar = ITEMS.register("realgar", 
+            ()-> new Item(new Item.Properties().group(ModTabGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> orpiment = ITEMS.register("orpiment", 
+            ()-> new Item(new Item.Properties().group(ModTabGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> arsenide_salt = ITEMS.register("arsenide_salt", 
+            ()-> new Item(new Item.Properties().group(ModTabGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> medium_arsenic_chunk = ITEMS.register("medium_arsenic_chunk", 
+            ()-> new Item(new Item.Properties().group(ModTabGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> large_arsenic_chunk = ITEMS.register("large_arsenic_chunk", 
+            ()-> new Item(new Item.Properties().group(ModTabGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> arsenide_bronze_ingot = ITEMS.register("arsenide_bronze_ingot", 
+            ()-> new Item(new Item.Properties().group(ModTabGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> arsenide_bronze_nugget = ITEMS.register("arsenide_bronze_nugget", 
+            ()-> new Item(new Item.Properties().group(ModTabGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> arsenide_bronze_dust = ITEMS.register("arsenide_bronze_dust", 
+            ()-> new Item(new Item.Properties().group(ModTabGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> medium_arsenide_bronze_chunk = ITEMS.register("medium_arsenide_bronze_chunk", 
+            ()-> new Item(new Item.Properties().group(ModTabGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> large_arsenide_bronze_chunk = ITEMS.register("large_arsenide_bronze_chunk", 
+            ()-> new Item(new Item.Properties().group(ModTabGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> arsenide_gold_ingot = ITEMS.register("arsenide_gold_ingot", 
+            ()-> new Item(new Item.Properties().group(ModTabGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> arsenide_gold_nugget = ITEMS.register("arsenide_gold_nugget", 
+            ()-> new Item(new Item.Properties().group(ModTabGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> arsenide_gold_dust = ITEMS.register("arsenide_gold_dust", 
+            ()-> new Item(new Item.Properties().group(ModTabGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> medium_arsenide_gold_chunk = ITEMS.register("medium_arsenide_gold_chunk", 
+            ()-> new Item(new Item.Properties().group(ModTabGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> large_arsenide_gold_chunk = ITEMS.register("large_arsenide_gold_chunk", 
+            ()-> new Item(new Item.Properties().group(ModTabGroups.MOD_ITEM_GROUP)));
+    
+    public static final RegistryObject<Item> tenebrium_ingot = ITEMS.register("tenebrium_ingot", 
+            ()-> new Item(new Item.Properties().group(ModTabGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> tenebrium_nugget = ITEMS.register("tenebrium_nugget", 
+            ()-> new Item(new Item.Properties().group(ModTabGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> tenebrium_dust = ITEMS.register("tenebrium_dust", 
+            ()-> new Item(new Item.Properties().group(ModTabGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> medium_tenebrium_chunk = ITEMS.register("medium_tenebrium_chunk", 
+            ()-> new Item(new Item.Properties().group(ModTabGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<Item> large_tenebrium_chunk = ITEMS.register("large_tenebrium_chunk", 
+            ()-> new Item(new Item.Properties().group(ModTabGroups.MOD_ITEM_GROUP)));
+    
 } // end class
