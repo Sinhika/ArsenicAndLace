df2811043b9a11f206879ba4dff4fcb1de496020
Sinhika <sinhika@republicofnewhome.org>
2021-05-31 16:37:23 -0500

    refactored config

--- src/main/java/mod/akkamaddi/arsenic/config/ArsenicConfig.java
+++ src/main/java/mod/akkamaddi/arsenic/config/ArsenicConfig.java
@@ -1,35 +1,12 @@
 package mod.akkamaddi.arsenic.config;
 
-import java.util.HashMap;
-import java.util.Map;
+import mod.alexndr.simplecorelib.config.SimpleConfig;
 
-import mod.alexndr.simplecorelib.config.ISimpleConfig;
-
-public class ArsenicConfig implements ISimpleConfig
+public class ArsenicConfig extends SimpleConfig
 {
     // recipe flags
-    private static Map<String, Boolean> flags = new HashMap<>();
     public static ArsenicConfig INSTANCE = new ArsenicConfig();
     
     public static boolean enableChestLoot;
     
-    @Override
-    public void clear()
-    {
-        flags.clear();
-    }
-
-    @Override
-    public boolean getFlag(String n)
-    {
-        Boolean obj = flags.get(n);
-        return obj != null && obj;
-    }
-
-    @Override
-    public void putFlag(String n, boolean val)
-    {
-        flags.put(n, val);
-    }
-
 } // end-class
